e53c12073565eeaea03dd2674421b97948b81733
Ahmed <ahmed.mano.am68@gmail.com>
2022-12-21 21:44:45 +0200

    modified: Java Inheritance I

--- HackerRank/03. Java/src/JavaInheritanceI.java
+++ HackerRank/03. Java/src/JavaInheritanceI.java
@@ -20,14 +20,3 @@ class Bird extends Animal{
     }
 }
 
-public class Solution{
-
-    public static void main(String args[]){
-
-        Bird bird = new Bird();
-        bird.walk();
-        bird.fly();
-        bird.sing();
-
-    }
-}
