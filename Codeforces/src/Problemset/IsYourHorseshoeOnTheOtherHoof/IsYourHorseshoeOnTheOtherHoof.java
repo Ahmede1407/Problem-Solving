d9ad5941f45c6182f71aaf53841c5462d0498889
Ahmed <ahmed.mano.am68@gmail.com>
2024-08-29 12:59:41 +0300

    A. Is your horseshoe on the other hoof?

---
+++ Codeforces/src/Problemset/IsYourHorseshoeOnTheOtherHoof/IsYourHorseshoeOnTheOtherHoof.java
@@ -0,0 +1,32 @@
+package Problemset.IsYourHorseshoeOnTheOtherHoof;
+// problem: https://codeforces.com/problemset/problem/228/A
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class IsYourHorseshoeOnTheOtherHoof {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        int minNumberOfHorseShoes = 0;
+
+        for (int i = 0; i < 4; i++) {
+            colors.add(scanner.nextInt());
+        }
+
+        Collections.sort(colors);
+
+        for (int i = 0; i < 3; i++) {
+            if (Objects.equals(colors.get(i), colors.get(i + 1))) {
+                minNumberOfHorseShoes++;
+            }
+        }
+
+        System.out.println(minNumberOfHorseShoes);
+        System.out.println(colors);
+    }
+}
